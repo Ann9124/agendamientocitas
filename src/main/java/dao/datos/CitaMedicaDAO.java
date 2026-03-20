e90d084189453562a46f92e1c7462b8e8a8e6a21
sarri <sarri@192.168.1.40>
2026-03-20 13:43:48 -0500

    inicializar base de datos

--- src/main/java/dao/datos/CitaMedicaDAO.java
+++ src/main/java/dao/datos/CitaMedicaDAO.java
@@ -1,114 +1,44 @@
-package dao.datos;
+package dao.dominio;
 
-import dao.dominio.CitaMedica;
-import java.sql.*;
-import java.util.*;
+import java.sql.ResultSet;
+import java.util.Date;
 
-public class CitaMedicaDAO {
+public class CitaMedica {
 
-    public static String SQL_SELECT = "SELECT * FROM CitaMedica";
-    public static String SQL_INSERT = "INSERT INTO CitaMedica(idcitamedica,idMedico) VALUES (?,?)";
-    public static String SQL_UPDATE = "UPDATE CitaMedica SET idCitaMedica=?,idMedico=? WHERE id=?";
-    public static String SQL_DELETE = "DELETE FROM Citamedica WHERE id=?";
+    private int id;
+    private int idPaciente;
+    private int idMedico;
 
-    public CitaMedicaDAO() {
+    public CitaMedica(ResultSet res) {
     }
 
-    public void imprimir() {
-        try {
-            List<CitaMedica> citamedicas = seleccionarCitaMedicas();
-            for (CitaMedica citamedica : citamedicas) {
-                System.out.println(citamedica);
-
-            }
-        } catch (SQLException e) {
-        }
-    }
-    //*MOSTRAR CitaMedicaDAO*//
-
-    public List<CitaMedica> seleccionarCitaMedicas() throws SQLException {
-        Connection conex = null;
-        PreparedStatement sentencia = null;
-        ResultSet res = null;
-        CitaMedicaDAO citamedica = null;
-
-        List<CitaMedica> citamedicas = new ArrayList<>();
-
-        try {
-            conex = ConexionBD.getconex();
-            sentencia = conex.prepareStatement(SQL_SELECT);
-            res = sentencia.executeQuery();
-
-            while (res.next()) {
-//                citamedica = new CitaMedicaDAO();
-//                citamedica.setId(res.getInt("id"));
-//                citamedica.setNombre(res.getString("nombre"));
-//                citamedica.setApellido(res.getString("apellido"));
-//                citamedica.setFechaNaci(res.getDate("fechaNaci"));
-//                citamedica.setTelefono(res.getString("telefono"));
-//
-//                citamedicas.add(citamedica);
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
-        }
-        return citamedicas;
+    public CitaMedica(int id, int idPaciente, int idMedico) {
+        this.id = id;
+        this.idPaciente = idPaciente;
+        this.idMedico = idMedico;
     }
 
-    //*INSERTAR CitaMedicaDAO*//
-    public void insertar(CitaMedicaDAO p) {
-
-        try {
-            Connection conex = ConexionBD.getconex();
-            PreparedStatement sentencia = conex.prepareStatement(SQL_INSERT);
-
-//            sentencia.setString(1, p.getNombre());
-//            sentencia.setString(2, p.getApellido());
-//            sentencia.setString(3, p.getTelefono());
-            sentencia.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
-        }
-
+    public int getId() {
+        return id;
     }
 
-    //*ACTUALIZAR CitaMedicaDAO*//       
-    public void actualizar(CitaMedicaDAO p) {
-
-        try {
-            Connection conex = ConexionBD.getconex();
-            PreparedStatement sentencia = conex.prepareStatement(SQL_UPDATE);
-
-//            sentencia.setString(1, p.getNombre());
-//            sentencia.setString(2, p.getApellido());
-//            sentencia.setString(3, p.getTelefono());
-//            sentencia.setInt(4,p.getId());
-            sentencia.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
-        }
+    public void setId(int id) {
+        this.id = id;
     }
-    //*ELIMINAR CitaMedicaDAO*//
 
-    public void eliminar(int id) {
+    public int getIdPaciente() {
+        return idPaciente;
+    }
 
-        try {
-            Connection conex = ConexionBD.getconex();
-            PreparedStatement sentencia = conex.prepareStatement(SQL_DELETE);
+    public void setIdPaciente(int idPaciente) {
+        this.idPaciente = idPaciente;
+    }
 
-            sentencia.setInt(1, id);
+    public int getIdMedico() {
+        return idMedico;
+    }
 
-            sentencia.executeUpdate();
-            sentencia.close();
-            conex.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
-
-        }
-
+    public void setIdMedico(int idMedico) {
+        this.idMedico = idMedico;
     }
 }
